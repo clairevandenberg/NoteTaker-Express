58b783480843337c04a418a475650b45b7718419
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-05-07 17:21:35 +0930

    added Get, Post & Delete route API requests

--- server.js
+++ server.js
@@ -9,7 +9,42 @@ let PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let notes = []; // empty array for storing notes 
 
+// GET route 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+    return res.json(notes);
+});
+
+
+// POST route 
+app.post("/api/notes", function(req, res) {
+    console.log(notes);
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    if (err) { 
+    throw err;
+    } else {
+    return res.json(notes);
+    };
+});
+
+
+//DELETE route 
+app.delete("/api/notes: id", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    if (err) { 
+    throw err;
+    } else {
+    return res.json(notes);
+    };
+});
 
 //start server to begin listening 
 app.listen(PORT, function () {
