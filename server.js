f7da03de66e4a17b2621bfec46e9249a569960a6
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-05-11 21:58:33 +0930

    User can now save notes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // creating express server using Express.JS
 let express = require("express");
 let path = require("path");
+const fs = require("fs");
 
 // setting up express app and Heroku hosting requirement 
 let app = express ();
@@ -11,35 +12,29 @@ app.use(express.json());
 
 let notes = []; // empty array for storing notes 
 
+
 // POST route 
 app.post(`/api/notes`, function(req, res) {
+let userNotes = fs.readFileSync(path.join(__dirname, "/db/db.json"));
+userNotes = JSON.parse(userNotes);
 
-    // push users note to notes array
-    notes.push(req.body);
-    let notes = userNotes;
-    
-    let userNotes =  fs.readFile(path.join(__dirname, "/db/db.json"));
+userNotes.push(req.body);
+userNotes = JSON.stringify(userNotes);
+console.log(userNotes);
 
-    JSON.parse(userNotes),
-    notes.push(notes);
-
-    userNotes = JSON.stringify(userNotes);
-    console.log(userNotes);
-
-    fs.writeFile(path.join(__dirname, "./db/db.json"), userNotes, `utf8`, function (error) {
-       if (err){
-        console.log( "You have an Error");
+fs.writeFile(path.join(__dirname, "./db/db.json"), userNotes, `utf8`, function (err) {
+    if (err){
+     console.log( "You have an Error");
     } else { 
-        return res.json(notes);
-        console.log("Your note has been saved");
+     console.log("Your note has been saved");
+    return res.json(notes);
         };
     });
 });
 
 
 // GET route 
-app.use(express.static('public')); 
-
+app.use(express.static('public'));
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -48,22 +43,23 @@ app.get("/notes", function(req, res) {
 });
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
-    return res.json(notes);
 });
-
 
 
 // DELETE route 
 app.delete("/api/notes: id", function(req, res) {
     fs.sendFile(path.join(__dirname, "/public/notes.html"));
-    if (err) { 
-    throw err;
-    } 
-    console.log(data)
-    let parseNotes = JSON.parse(data);
-    console.log (parseNotes);
-    res.send(parseNotes);
+        if (err) { 
+        throw err;
+        } 
+
+ console.log(data)
+   
+let parseNotes = JSON.parse(data);
+console.log (parseNotes);
+res.send(parseNotes);
 });
+
 
 // start server to begin listening 
 app.listen(PORT, function () {
